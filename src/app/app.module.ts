de3585e83d2e73052e92bfc7f6290367e807aba4
dymafr <erwan1309@hotmail.com>
2020-12-28 14:31:21 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { CocktailListComponent } from "./cocktail-container/cocktail-list/cocktail-list.component";
+import { CocktailDetailsComponent } from "./cocktail-container/cocktail-details/cocktail-details.component";
+import { CocktailContainerComponent } from "./cocktail-container/cocktail-container.component";
+import { SelectedDirective } from "./shared/directives/selected.directive";
+import { PanierContainerComponent } from "./panier-container/panier-container.component";
+import { IngredientListComponent } from "./panier-container/ingredient-list/ingredient-list.component";
+import { RouterModule } from "@angular/router";
+import { APP_ROUTES } from "./app.routes";
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(APP_ROUTES)],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CocktailListComponent,
+    CocktailDetailsComponent,
+    CocktailContainerComponent,
+    SelectedDirective,
+    PanierContainerComponent,
+    IngredientListComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: []
+})
+export class AppModule {}
