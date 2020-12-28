4a2b1a69e03f3fa268eed149f81fd7f38fc6c2a6
dymafr <erwan1309@hotmail.com>
2020-12-28 14:55:28 +0100

    1 file modified

--- src/app/panier-container/panier-container.component.ts
+++ src/app/panier-container/panier-container.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Ingredient } from "../shared/interfaces/ingredient.interface";
+import { PanierService } from "../shared/services/panier.service";
 
 @Component({
   selector: "app-panier-container",
   templateUrl: "./panier-container.component.html",
   styleUrls: ["./panier-container.component.scss"]
 })
-export class PanierContainerComponent implements OnInit {
-  constructor() {}
+export class PanierContainerComponent implements OnInit, OnDestroy {
+  public ingredients: Ingredient[];
+  public subscription: Subscription;
 
-  ngOnInit() {}
+  constructor(private panierService: PanierService) {}
+
+  ngOnInit() {
+    this.subscription = this.panierService.ingredients$.subscribe(
+      (ingredients: Ingredient[]) => (this.ingredients = ingredients)
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
