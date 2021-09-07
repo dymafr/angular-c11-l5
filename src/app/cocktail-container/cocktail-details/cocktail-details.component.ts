b6c3567b2aa81a329efb186a390fcc243dc96f71
dymafr <erwan1309@hotmail.com>
2021-09-07 14:19:39 +0200

    7 files modified

--- src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Cocktail } from "../../shared/interfaces/cocktail.interface";
-import { PanierService } from "../../shared/services/panier.service";
+import { Component, OnInit, Input } from '@angular/core';
+import { Cocktail } from '../../shared/interfaces/cocktail.interface';
+import { PanierService } from '../../shared/services/panier.service';
 
 @Component({
-  selector: "app-cocktail-details",
-  templateUrl: "./cocktail-details.component.html",
-  styleUrls: ["./cocktail-details.component.scss"]
+  selector: 'app-cocktail-details',
+  templateUrl: './cocktail-details.component.html',
+  styleUrls: ['./cocktail-details.component.scss']
 })
 export class CocktailDetailsComponent implements OnInit {
-  @Input() cocktail: Cocktail;
+  @Input() cocktail?: Cocktail;
 
   constructor(private panierService: PanierService) {}
 
   ngOnInit(): void {}
 
   public addToPanier() {
-    this.panierService.addPanier(this.cocktail.ingredients);
+    if (this.cocktail) {
+      this.panierService.addPanier(this.cocktail.ingredients);
+    }
   }
 }
